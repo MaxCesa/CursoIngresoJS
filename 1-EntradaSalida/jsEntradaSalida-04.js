8c8bce946dee973709aa5b295363eba435d46c4c
MaxCesa <maxicesa00@gmail.com>
2022-07-04 23:23:52 -0300

    ejercicios 1 a 5 resueltos

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,8 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	let nombre = prompt("Ingrese nombre")
+	txtIdNombre.value = nombre 
+	alert(document.getElementById(nombre).value);
 }
 
